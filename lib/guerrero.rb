cc62d233e75b7d4768a4739a8a8e6f9b6748196a
Facundo Javier Gelatti <javiergelatti@gmail.com>
2020-04-12 22:34:58 -0300

    Hacer que Guerrero subclasifique a Espadachin

--- lib/guerrero.rb
+++ lib/guerrero.rb
@@ -1,23 +1,5 @@
-class Guerrero
-  attr_reader :vida
-  attr_reader :defensa
-
-  def initialize(fuerza:, defensa:, vida:)
-    @fuerza = fuerza
-    @defensa = defensa
-    @vida = vida
-  end
-
-  def atacar_a(otra_unidad)
-    otra_unidad.recibir_daño(ataque)
-  end
-
+class Guerrero < Espadachin
   def ataque
     @fuerza
-  end
-
-  def recibir_daño(cantidad_de_daño)
-    daño_final = cantidad_de_daño - @defensa
-    @vida -= daño_final if daño_final > 0
   end
 end
